9e81500ab9b806c5de2fd0fd6baa31b912b82e36
mounforjs <mounford.js@gmail.com>
2026-04-06 11:06:27 -0400

    Evaluacion work 1

--- app/Filament/Resources/Evaluacions/Schemas/EvaluacionForm.php
+++ app/Filament/Resources/Evaluacions/Schemas/EvaluacionForm.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Resources\Evaluacions\Schemas;
 
+use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class EvaluacionForm
@@ -10,10 +12,28 @@ class EvaluacionForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->components([
-                TextInput::make('descripcion')
-                    ->required(),
-                TextInput::make('nombre_corto'),
+            ->schema([
+                Section::make("Evaluacion")
+                ->schema([
+                    TextInput::make('nombre_corto'),
+                    RichEditor::make('descripcion')
+                        ->required()
+                        ->toolbarButtons([
+                            'blockquote',
+                            'bold',
+                            'bulletList',
+                            'codeBlock',
+                            'h2',
+                            'h3',
+                            'italic',
+                            'link',
+                            'orderedList',
+                            'redo',
+                            'strike',
+                            'undo',
+                        ])
+                    
+                ])
             ]);
     }
 }
--- database/migrations/2026_04_04_153407_create_evaluaciones_table.php
+++ database/migrations/2026_04_04_153407_create_evaluaciones_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('evaluaciones', function (Blueprint $table) {
             $table->id();
-            $table->string("descripcion");
+            $table->longText("descripcion");
             $table->string("nombre_corto")->nullable();
             $table->timestamp("created_at")->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
