93582abc9c8100d25a219bace2058f23c431431e
mounforjs <mounford.js@gmail.com>
2026-04-07 15:31:27 -0400

    Select email validation, roles with checklist on user form

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\View;
 use Filament\Schemas\Schema;
+use Illuminate\Support\HtmlString;
 
 class UserForm
 {
@@ -16,16 +20,34 @@ class UserForm
                 Section::make('Registro')
                     ->description('Datos del usuario')
                     ->schema([
-                        TextInput::make('name')
+                        TextInput::make('Nombre')
                             ->required(),
                         TextInput::make('email')
                             ->label('Email address')
                             ->email()
-                            ->required(),
+                            ->required()
+                            ->live()
+                            ->unique(ignoreRecord: True, table: "users", column: "email")
+                            ->debounce(500)
+                            ->validationMessages(['unique' => 'Este correo ya ha sido registrado']),
                         TextInput::make('password')
                             ->password()
                             ->required(),
-                    ])
+                        
+                        //View::make('filament.components.divider')
+                         //   ->columnSpanFull(),
+            
+                        
+                    ]),
+            
+                Section::make("Roles")
+                    ->schema([
+                        CheckboxList::make('roles')
+                            ->relationship('roles', 'rol') // 'roles' es el nombre de la relación en el modelo User
+                            ->searchable() // Opcional: si tienes muchos roles
+                            ->columns(2) // Organiza los checks en 2 columnas
+                            ->gridDirection('vertical'),
+                    ])->columnStart(2)
             ]);
     }
 }
