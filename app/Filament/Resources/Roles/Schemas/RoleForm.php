29a8fd5878c23e71feb1e1befbdd6dbcea0109e6
mounforjs <mounford.js@gmail.com>
2026-04-07 16:42:18 -0400

    Refactor RoleForm schema configuration to include section and description for role

--- app/Filament/Resources/Roles/Schemas/RoleForm.php
+++ app/Filament/Resources/Roles/Schemas/RoleForm.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Roles\Schemas;
 
+use Dom\Text;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class RoleForm
@@ -9,8 +13,16 @@ class RoleForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->components([
-                //
+            ->schema([
+                Section::make('Registro')
+                    ->description('Datos del Rol')
+                    ->schema([
+                TextInput::make('rol')
+                    ->required()
+                    ->label('Nombre'),
+                Textarea::make('description')
+                    ->label('Descripcion'),
+                ])
             ]);
     }
 }
